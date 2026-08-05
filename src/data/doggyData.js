356da70e47d3ae82a8240de013e6b2dbf7551ef0
wow1324332 <wow1324332@gmail.com>
2026-08-05 16:17:08 +0900

    Create doggyData.js

---
+++ src/data/doggyData.js
@@ -0,0 +1,13 @@
+// src/data/doggyData.js
+export const doggyData = [
+  {
+    id: 1,
+    name: "세이블 포메라니안",
+    image: "/ezgif-8ab4d8c3c611b2e0.webp" // 우리가 방금 만든 투명 꾸루
+  },
+  {
+    id: 2,
+    name: "파티 포메라니안1",
+    image: "/pomeranian-party.webp" // 임시 이미지 (나중에 바꾸시면 됩니다!)
+  }
+];

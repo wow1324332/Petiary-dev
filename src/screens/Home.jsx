d8e1cde303f720fd1a534929670b02164bd1a0bd
wow1324332 <wow1324332@gmail.com>
2026-08-05 09:35:04 +0900

    Create Home.jsx

---
+++ src/screens/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col h-full p-5 items-center justify-center">
+      <h2 className="text-2xl font-bold text-brand mb-2">내 펫의 방 🐾</h2>
+      <p className="text-base text-gray-500 text-center">여기에 귀여운 꾸루의 방이 꾸며질 예정입니다.</p>
+    </div>
+  );
+}

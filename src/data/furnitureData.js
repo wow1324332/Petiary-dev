f9afcb4c6317785d0759a024a7d8e2975633ed65
wow1324332 <wow1324332@gmail.com>
2026-08-06 08:21:36 +0900

    Update furnitureData.js

--- src/data/furnitureData.js
+++ src/data/furnitureData.js
@@ -3,6 +3,6 @@ export const furnitureData = [
     id: 1,
     name: "베이직 소파",
     // 🌟 첨부해주신 이미지 파일명을 그대로 사용합니다. (public 폴더에 넣어주세요!)
-    image: "/furniture-sofa-bagic.png" 
+    image: "/furniture/furniture-sofa-bagic.png" 
   }
 ];

ae45eab240867cd0a942d850e0b102fd16ca3159
wow1324332 <wow1324332@gmail.com>
2026-08-05 09:17:13 +0900

    Add Firebase configuration and initialization

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnTprZv08X1WzjLPzCJeBUPLk2UHE_LHk",
+  authDomain: "petiary-dev.firebaseapp.com",
+  projectId: "petiary-dev",
+  storageBucket: "petiary-dev.firebasestorage.app",
+  messagingSenderId: "1095114770746",
+  appId: "1:1095114770746:web:4990078ee70aa98ec6e803"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);

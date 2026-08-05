ed8a7d2fb973276670ce8015528d35becda580dd
wow1324332 <wow1324332@gmail.com>
2026-08-06 08:21:02 +0900

    Update backgroundData.js

--- src/data/backgroundData.js
+++ src/data/backgroundData.js
@@ -3,21 +3,21 @@ export const backgroundData = [
     id: 1,
     name: "기본 방",
     // 첨부해주신 원본 파일명을 그대로 사용합니다. (public 폴더에 넣어주세요!)
-    image: "/home-bg-basic.jpg" 
+    image: "/bg/home-bg-basic.jpg" 
   },
   {
     id: 2,
     name: "핑크 룸",
-    image: "/home-bg-pinkroom.jpg" // 나중에 바꿀 임시 이미지
+    image: "/bg/home-bg-pinkroom.jpg" // 나중에 바꿀 임시 이미지
   },
   {
     id: 3,
     name: "애견카페",
-    image: "/home-bg-forest.jpg" // 나중에 바꿀 임시 이미지
+    image: "/bg/home-bg-forest.jpg" // 나중에 바꿀 임시 이미지
   },
   {
     id: 4,
     name: "모던 아파트",
-    image: "/home-bg-modernapt.jpg"
+    image: "/bg/home-bg-modernapt.jpg"
   }
 ];

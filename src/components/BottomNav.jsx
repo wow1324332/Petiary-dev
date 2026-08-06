46a86549e2a81b6991aa45317f2960813f5e4d19
wow1324332 <wow1324332@gmail.com>
2026-08-07 08:04:44 +0900

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -11,9 +11,11 @@ export default function BottomNav() {
         <Home size={24} className={location.pathname === '/' ? 'text-brand' : 'text-gray-300'} />
         <span className={`text-xs mt-1 font-medium ${location.pathname === '/' ? 'text-brand' : 'text-gray-400'}`}>마이룸</span>
       </Link>
-      <Link to="/feed" className="flex-1 flex flex-col items-center justify-center transition-colors">
-        <BookImage size={24} className={location.pathname === '/Diary' ? 'text-brand' : 'text-gray-300'} />
-        <span className={`text-xs mt-1 font-medium ${location.pathname === '/Diary' ? 'text-brand' : 'text-gray-400'}`}>일기장</span>
+      
+      {/* 👇 클릭 시 이동 주소(to)와 색칠 기준(pathname)을 모두 소문자 /diary 로 완벽하게 통일했습니다! */}
+      <Link to="/diary" className="flex-1 flex flex-col items-center justify-center transition-colors">
+        <BookImage size={24} className={location.pathname === '/diary' ? 'text-brand' : 'text-gray-300'} />
+        <span className={`text-xs mt-1 font-medium ${location.pathname === '/diary' ? 'text-brand' : 'text-gray-400'}`}>일기장</span>
       </Link>
     </div>
   );

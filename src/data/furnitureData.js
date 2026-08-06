5e7ea694bdc55f8e1313dacfe2ed8efe50266718
wow1324332 <wow1324332@gmail.com>
2026-08-06 16:35:46 +0900

    Update furnitureData.js

--- src/data/furnitureData.js
+++ src/data/furnitureData.js
@@ -5,16 +5,21 @@ export const furnitureData = [
     // 🌟 첨부해주신 이미지 파일명을 그대로 사용합니다. (public 폴더에 넣어주세요!)
     image: "/furniture/furniture-sofa-bagic.png" 
   },
-    {
+  {
     id: 2,
     name: "한강뷰 샷시",
     // 🌟 첨부해주신 이미지 파일명을 그대로 사용합니다. (public 폴더에 넣어주세요!)
     image: "/furniture/furniture-샷시-한강뷰.png" 
   },
-    {
+  {
     id: 3,
     name: "한강뷰 통창",
     // 🌟 첨부해주신 이미지 파일명을 그대로 사용합니다. (public 폴더에 넣어주세요!)
     image: "/furniture/furniture-통창-한강뷰.png" 
+  },
+  {
+    id: 4,
+    name: "흰커튼 펄럭임",
+    image: "/furniture/furniture-커튼-흰색-펄럭임.png"
   }
 ];

8e086db49125591a1d298fcc14461222ce078bf0
wow1324332 <wow1324332@gmail.com>
2026-08-06 17:57:12 +0900

    Update toastData.js

--- src/data/toastData.js
+++ src/data/toastData.js
@@ -1,14 +1,14 @@
 export const toastMessages = {
   lockedFurn: {
     message: "잠금을 해제하세요!",
-    style: "bg-blue-50/90 text-blue-700 border-blue-200" // 펫은 귀여운 파란색 테마
+    style: "bg-blue-300/30 text-blue-900 border-blue-200/50" // 펫은 귀여운 파란색 테마
   },
   lockedDog: {
     message: "잠금을 해제하세요!",
-    style: "bg-blue-50/90 text-blue-700 border-blue-200" // 펫은 귀여운 파란색 테마
+    style: "bg-blue-300/30 text-blue-900 border-blue-200/50" // 펫은 귀여운 파란색 테마
   },
   lockedBg: {
     message: "잠금을 해제하세요!",
-    style: "bg-blue-50/90 text-blue-700 border-blue-200" // 펫은 귀여운 파란색 테마
+    style: "bg-blue-300/30 text-blue-900 border-blue-200/50" // 펫은 귀여운 파란색 테마
   }
 };

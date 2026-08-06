fd269eaa9aa39c45adfaf279d7e23eaac516c191
wow1324332 <wow1324332@gmail.com>
2026-08-07 07:58:24 +0900

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -12,8 +12,8 @@ export default function BottomNav() {
         <span className={`text-xs mt-1 font-medium ${location.pathname === '/' ? 'text-brand' : 'text-gray-400'}`}>마이룸</span>
       </Link>
       <Link to="/feed" className="flex-1 flex flex-col items-center justify-center transition-colors">
-        <BookImage size={24} className={location.pathname === '/feed' ? 'text-brand' : 'text-gray-300'} />
-        <span className={`text-xs mt-1 font-medium ${location.pathname === '/feed' ? 'text-brand' : 'text-gray-400'}`}>일기장</span>
+        <BookImage size={24} className={location.pathname === '/Diary' ? 'text-brand' : 'text-gray-300'} />
+        <span className={`text-xs mt-1 font-medium ${location.pathname === '/Diary' ? 'text-brand' : 'text-gray-400'}`}>일기장</span>
       </Link>
     </div>
   );
